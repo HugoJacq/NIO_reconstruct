fixed M matrix computation

src/models/classic_slab.py
@@ -154,7 +154,7 @@ class jslab_kt(eqx.Module):
         self.dt = dt
         
         self.dTK = dTK
-        self.NdT = int((t1-t0)//dTK) # jnp.array((t1-t0)//self.dTK,int)
+        self.NdT = NdT = len(np.arange(t0, t1+dTK,dTK)) # int((t1-t0)//dTK)  
         self.pk = pk #self.kt_ini( jnp.asarray(pk) )
         
         self.TAx = TAx
@@ -183,11 +183,9 @@ class jslab_kt(eqx.Module):
         t0, t1, dt = self.t0, self.t1, self.dt # call_args
         nsubsteps = self.dt_forcing // dt
         # control
-        # K = jnp.exp( jnp.asarray(self.pk) )
-        # K = self.kt_ini(K)
         K = jnp.exp( self.pk) 
         K = kt_1D_to_2D(K, NdT=self.NdT, nl=self.nl)
-        forcing_time = jnp.arange(t0,t1,self.dt_forcing)
+        forcing_time = np.arange(t0,t1,self.dt_forcing)
         M = pkt2Kt_matrix(gtime=forcing_time, NdT=self.NdT, dTK=self.dTK)
         Kt = jnp.dot(M,K)
         args = self.fc, Kt, self.TAx, self.TAy
@@ -287,32 +285,59 @@ def pkt2Kt_matrix(NdT, dTK, gtime):
         vector_Kt :
             X - - - - * - - - - * - - - - * - - - - * X  # 6 values
         """
-        gptime = jnp.zeros(NdT) #self.ntm//step+1 )        
+        # gptime = jnp.zeros(NdT) #self.ntm//step+1 )        
+        # dt_gtime = gtime[1]-gtime[0]
+        # step = np.int32(dTK//dt_gtime)
+        # for ipt in range(len(gptime)):
+        #     gptime = gptime.at[ipt].set( gtime[ipt*step])
+        # gptime = gptime.at[-1].set( lax.select(len(gptime)>0, gtime[-1]+dTK, gtime[0]) )
+        # gptime = gptime + dTK
 
-        nt = len(gtime)
+
+        # nt = len(gtime)
+        # npt = len(gptime)
+        # M = jnp.zeros((nt,npt))
+        # S = jnp.zeros((nt))
+
+        # def __step_pkt2Kt_matrix(arg0, ip):
+        #     gtime,gptime,S,M = arg0
+        #     distt = (gtime-gptime[ip])
+        #     tmp =  jnp.exp(-distt**2/dTK**2)
+        #     S = lax.add( S, tmp )
+        #     M = M.at[:,ip].set( M[:,ip] + tmp )
+        #     arg0 = gtime,gptime,S,M
+        #     return arg0,arg0
+
+        # # print('M.shape',M.shape)
+        # # print('S.shape',S.shape)
+        # # loop over each dT
+        # arg0 = gtime, gptime, S, M
+        # # This should work but there is a bug in jax
+        # # see : https://docs.kidger.site/equinox/faq/#how-to-use-non-array-modules-as-inputs-to-scancondwhile-etc
+        # # |
+        # # v
+        # #_, _, S, M = lax.fori_loop(0, npt, self.__step_pkt2Kt_matrix, arg0) 
+        # arg0,_ = lax.scan(lambda it,arg0: __step_pkt2Kt_matrix(it,arg0), arg0, xs=jnp.arange(0,npt))
+        # _, _, S, M = arg0
+        # M = (M.T / S.T).T
+        if dTK<gtime[-1]-gtime[0] : 
+            gptime = np.arange(gtime[0], gtime[-1]+dTK,dTK)
+        else: 
+            gptime = np.array([gtime[0]])
+        nt=len(gtime)
         npt = len(gptime)
-        M = jnp.zeros((nt,npt))
-        S = jnp.zeros((nt))
-
-        def __step_pkt2Kt_matrix(arg0, ip):
-            gtime,gptime,S,M = arg0
+        # print(gtime[-1]/86400, gtime[0]/86400,dTK/86400)
+        # print(gptime/86400)
+        # print(len(np.arange(gtime[0], gtime[-1],dTK)))
+        # print(npt)
+        M = np.zeros((nt,npt))
+        # Ks=np.zeros((ny,nx))
+        S=np.zeros((nt))
+        for ip in range(npt):
             distt = (gtime-gptime[ip])
-            tmp =  jnp.exp(-distt**2/dTK**2)
-            S = lax.add( S, tmp )
-            M = M.at[:,ip].set( M[:,ip] + tmp )
-            arg0 = gtime,gptime,S,M
-            return arg0,arg0
-
-        # print('M.shape',M.shape)
-        # print('S.shape',S.shape)
-        # loop over each dT
-        arg0 = gtime, gptime, S, M
-        # This should work but there is a bug in jax
-        # see : https://docs.kidger.site/equinox/faq/#how-to-use-non-array-modules-as-inputs-to-scancondwhile-etc
-        # |
-        # v
-        #_, _, S, M = lax.fori_loop(0, npt, self.__step_pkt2Kt_matrix, arg0) 
-        arg0,_ = lax.scan(lambda it,arg0: __step_pkt2Kt_matrix(it,arg0), arg0, xs=jnp.arange(0,npt))
-        _, _, S, M = arg0
+            iit = np.where((np.abs(distt) < (3*dTK)))[0]
+            tmp = np.exp(-distt[iit]**2/dTK**2)
+            S[iit] += tmp
+            M[iit,ip] += tmp
         M = (M.T / S.T).T
         return M

tests_models/run_tests.py
@@ -8,10 +8,9 @@ import time as clock
 import matplotlib.pyplot as plt
 import sys
 import os
-#sys.path.append('..')
 sys.path.insert(0, '../src')
 
-from models.classic_slab import jslab,jslab_kt, kt_ini
+from models.classic_slab import jslab, jslab_kt, kt_ini, kt_1D_to_2D, pkt2Kt_matrix
 import forcing
 import inv
 import observations
@@ -32,13 +31,13 @@ AD_mode             = 'F'       # forward mode for AD
 
 # run parameters
 t0                  = 0.
-t1                  = 28*86400. #
+t1                  = 28*86400. 
 dt                  = 60.        # timestep of the model (s) 
 
 # What to test
-FORWARD_PASS        = True      # tests forward, cost, gradcost
-MINIMIZE            = False      # switch to do the minimisation process
-maxiter             = 100       # max number of iteration
+FORWARD_PASS        = False      # tests forward, cost, gradcost
+MINIMIZE            = True      # switch to do the minimisation process
+maxiter             = 50         # max number of iteration
 
 # Switches
 TEST_SLAB                   = False
@@ -156,7 +155,7 @@ if __name__ == "__main__":
         print('* test jslab_kt with filter at fc before computing cost')
         # control vector
         pk = jnp.asarray([-11.31980127, -10.28525189])   
-        NdT = int((t1-t0)//dTK) 
+        NdT = len(np.arange(t0, t1+dTK,dTK)) # int((t1-t0)//dTK) 
         pk = kt_ini(pk, NdT)
         
         # pk = jnp.asarray([-11.31980127, -11.31980127, -11.31980127, -11.31980127, -11.31980127, # dTK = 3*86400
@@ -181,9 +180,23 @@ if __name__ == "__main__":
             t7 = clock.time()
             mymodel = var_dfx.scipy_lbfgs_wrapper(mymodel, maxiter, gtol=1e-5, verbose=True)   
             print(' time, minimize',clock.time()-t7)
-                           
+        
+        M = pkt2Kt_matrix(NdT, dTK, np.arange(t0,t1,dt_forcing))
+        kt2D = kt_1D_to_2D(mymodel.pk, NdT, Nl)
+        new_kt = np.dot(M,kt2D)
+        fig, ax = plt.subplots(1,1,figsize = (10,3),constrained_layout=True,dpi=dpi)
+        for k in range(new_kt.shape[-1]):
+            #ax.plot( M[:,k] ) 
+            ax.plot(new_kt[:,k],label='K'+str(k))
+        ax.legend()              
         name_save = 'jslab_kt_'+namesave_loc
         plot_traj(mymodel, var_dfx, forcing1D, observations1D, name_save, path_save_png, dpi)
+        
+        fig, ax = plt.subplots(1,1,figsize = (10,3),constrained_layout=True,dpi=dpi)
+        ax.plot(forcing1D.time/86400, 1/np.exp(new_kt[:,0])/1000, label='estimated')
+        ax.plot(forcing1D.time/86400, forcing1D.MLD,label='true')
+        ax.set_ylabel('MLD (m)')
+        ax.set_xlabel('time (days)')
         
     end = clock.time()
     print('Total execution time = '+str(jnp.round(end-start,2))+' s')