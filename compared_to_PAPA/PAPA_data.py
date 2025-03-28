c0b5fbe0d2ba626f5886d7e25276a3e984e8b540
HugoJacq <hjacquet974@gmail.com>
2025-03-28 09:24:23 +0100

    up

--- compared_to_PAPA/PAPA_data.py
+++ compared_to_PAPA/PAPA_data.py
@@ -1,4 +1,10 @@
 """
 here : https://osmc.noaa.gov/erddap/tabledap/OSMC_flattened.html
 platform_code = 4800400
+
+above doesnt work
+
+here : https://www.pmel.noaa.gov/ocs/data/disdel/
+data in data_papa_2018
+
 """
